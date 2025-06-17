3cb6b611d96f9b9e68eb497fe2cd5e839693ed77
Cape23180909 <samilgamer@outlook.com>
2025-06-17 18:20:43 -0400

    Colcoando los nombres correctos al RemoteDataSource

--- app/src/main/java/edu/ucne/ureserve/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/RemoteDataSource.kt
@@ -3,18 +3,18 @@ package edu.ucne.ureserve.data.remote
 import edu.ucne.ureserve.data.remote.dto.UsuarioDTO
 import javax.inject.Inject
 
-class RemoteDataSource @Inject constructor (
+class RemoteDataSource @Inject constructor(
     private val api: UsuarioApi
-){
-    suspend fun getUsuarios(): List<UsuarioDTO> = api.getUsuarios()
+) {
+    suspend fun getUsuarios(): List<UsuarioDTO> = api.getAll()
 
-    suspend fun createUsuario(laboratorio: UsuarioDTO): UsuarioDTO =
-        api.createUsuarios(laboratorio)
+    suspend fun createUsuario(usuario: UsuarioDTO): UsuarioDTO =
+        api.insert(usuario)
 
-    suspend fun getUsuario(id: Int): UsuarioDTO = api.getUsuario(id)
+    suspend fun getUsuario(id: Int): UsuarioDTO = api.getById(id)
 
-    suspend fun updateUsuario(id: Int, laboratorio: UsuarioDTO): UsuarioDTO =
-        api.updateUsuario(id, laboratorio)
+    suspend fun updateUsuario(id: Int, usuario: UsuarioDTO): UsuarioDTO =
+        api.update(id, usuario)
 
-    suspend fun deleteUsuario(id: Int) = api.deleteUsuarios(id)
+    suspend fun deleteUsuario(id: Int) = api.delete(id)
 }
