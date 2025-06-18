aad24140e705b2167e7c3cfce23acebfecad9c33
Cape23180909 <samilgamer@outlook.com>
2025-06-18 16:38:40 -0400

    Terminando de hacer el dto, de Estudiantes

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/EstudianteDto.kt
@@ -0,0 +1,8 @@
+package edu.ucne.ureserve.data.remote.dto
+
+data class EstudianteDto (
+    val estudianteId: Int = 0,
+    val matricula: String = "",
+    val facultad: String = "",
+    val carrera: String = ""
+)
