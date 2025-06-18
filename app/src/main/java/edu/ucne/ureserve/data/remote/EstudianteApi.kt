1818f6cef720531a45e9f8e39ecef7e8dfa71e05
Cape23180909 <samilgamer@outlook.com>
2025-06-18 16:37:49 -0400

    Terminando de hacer EstudianteApi

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/EstudianteApi.kt
@@ -0,0 +1,27 @@
+package edu.ucne.ureserve.data.remote
+
+import edu.ucne.ureserve.data.remote.dto.EstudianteDto
+import edu.ucne.ureserve.data.remote.dto.UsuarioDTO
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface EstudianteApi {
+    @GET("api/Estudiantes")
+    suspend fun getAll(): List<EstudianteDto>
+
+    @GET("api/Estudiantes/{id}")
+    suspend fun getById(@Path("id") id: Int): EstudianteDto
+
+    @POST("api/Estudiantes")
+    suspend fun insert(@Body estudiante: EstudianteDto): EstudianteDto
+
+    @PUT("api/Estudiantes/{id}")
+    suspend fun update(@Path("id") id: Int, @Body estudiante: EstudianteDto): EstudianteDto
+
+    @DELETE("api/Estudiantes/{id}")
+    suspend fun delete(@Path("id") id: Int)
+}
