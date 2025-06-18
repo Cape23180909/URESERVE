5515d4728f9a15d7333f01d20b0d8e7afef42ac7
Cape23180909 <samilgamer@outlook.com>
2025-06-18 00:17:19 -0400

    Agregando las navegacion Ideales, para el cierre de sesion.

--- app/src/main/java/edu/ucne/ureserve/presentation/navigation/UreserveNavHost.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/navigation/UreserveNavHost.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.composable
 import edu.ucne.ureserve.presentation.dashboard.DashboardScreen
 import edu.ucne.ureserve.presentation.login.LoadStartScreen
 import edu.ucne.ureserve.presentation.login.LoginScreen
+import edu.ucne.ureserve.presentation.login.ProfileScreen
 
 @Composable
 fun UreserveNavHost(navController: NavHostController) {
@@ -34,7 +35,32 @@ fun UreserveNavHost(navController: NavHostController) {
             )
         }
         composable("Dashboard") {
-            DashboardScreen()
+            DashboardScreen(
+                onBottomNavClick = { destination ->
+                    when(destination) {
+                        "Perfil" -> navController.navigate("Profile")
+                        "Inicio" -> navController.navigate("Dashboard")
+                        "Tutorial" -> navController.navigate("Tutorial")
+                    }
+                }
+            )
         }
+        composable("Profile") {
+            ProfileScreen(
+                onLogout = {
+                    navController.navigate("LoadStart") {
+                        popUpTo("Profile") { inclusive = true }
+                    }
+                },
+                onBottomNavClick = { destination ->
+                    when(destination) {
+                        "Perfil" -> navController.navigate("Profile")
+                        "Inicio" -> navController.navigate("Dashboard")
+                        "Tutorial" -> navController.navigate("Tutorial")
+                    }
+                }
+            )
+        }
+        // Agrega aquí la pantalla de Tutorial si la tienes
     }
 }
