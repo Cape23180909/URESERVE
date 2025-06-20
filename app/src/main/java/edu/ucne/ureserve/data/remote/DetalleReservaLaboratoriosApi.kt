f645011444a31bf73484f60d07b039ea9e0bd6aa
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 19:16:04 -0400

    DetalleReservaLaboratoriosApi.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/DetalleReservaLaboratoriosApi.kt
@@ -0,0 +1,21 @@
+package edu.ucne.ureserve.data.remote
+
+import edu.ucne.ureserve.data.remote.dto.DetalleReservaLaboratoriosDto
+import retrofit2.http.*
+
+interface DetalleReservaLaboratoriosApi {
+    @GET("api/DetalleReservaLaboratorios")
+    suspend fun getAll(): List<DetalleReservaLaboratoriosDto>
+
+    @GET("api/DetalleReservaLaboratorios/{id}")
+    suspend fun getById(@Path("id") id: Int): DetalleReservaLaboratoriosDto
+
+    @POST("api/DetalleReservaLaboratorios")
+    suspend fun insert(@Body detalle: DetalleReservaLaboratoriosDto): DetalleReservaLaboratoriosDto
+
+    @PUT("api/DetalleReservaLaboratorios/{id}")
+    suspend fun update(@Path("id") id: Int, @Body detalle: DetalleReservaLaboratoriosDto): DetalleReservaLaboratoriosDto
+
+    @DELETE("api/DetalleReservaLaboratorios/{id}")
+    suspend fun delete(@Path("id") id: Int)
+}
