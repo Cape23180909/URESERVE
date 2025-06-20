1f7ce0ae48080ce8d9e277fd896b7e4f953b1cff
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 16:49:56 -0400

    ReportesDto.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/ReportesDto.kt
@@ -0,0 +1,14 @@
+package edu.ucne.ureserve.data.remote.dto
+
+
+data class ReportesDto(
+    val reporteId: Int = 0,
+    val tipoReporte: Int = 0,
+    val fechaInicio: String = "",
+    val fechaFin: String = "",
+    val fechaGeneracion: String = "",
+    val generadoPor: String = "",
+    val totalReservas: Int = 0,
+    val reservasActivas: Int = 0,
+    val reservasCanceladas: Int = 0
+)
