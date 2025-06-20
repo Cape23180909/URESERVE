8823cd55f277ff533baa85ad5872bb262c8980b4
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 19:17:01 -0400

    DetalleReservaProyectorsRepository.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/repository/DetalleReservaProyectorsRepository.kt
@@ -0,0 +1,40 @@
+package edu.ucne.ureserve.data.repository
+
+import android.util.Log
+import edu.ucne.ureserve.data.remote.RemoteDataSource
+import edu.ucne.ureserve.data.remote.Resource
+import edu.ucne.ureserve.data.remote.dto.DetalleReservaProyectorsDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class DetalleReservaProyectorsRepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) {
+    fun getDetalleReservaProyectors(): Flow<Resource<List<DetalleReservaProyectorsDto>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val detalles = remoteDataSource.getDetalleReservaProyectors()
+            emit(Resource.Success(detalles))
+        } catch (e: HttpException) {
+            Log.e("Retrofit", "HTTP error: ${e.message}", e)
+            emit(Resource.Error("Error de internet: ${e.message}"))
+        } catch (e: Exception) {
+            Log.e("Retrofit", "Unknown error: ${e.message}", e)
+            emit(Resource.Error("Error desconocido: ${e.message}"))
+        }
+    }
+
+    suspend fun getDetalleReservaProyector(id: Int): DetalleReservaProyectorsDto =
+        remoteDataSource.getDetalleReservaProyector(id)
+
+    suspend fun createDetalleReservaProyector(detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto =
+        remoteDataSource.createDetalleReservaProyector(detalle)
+
+    suspend fun updateDetalleReservaProyector(detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto =
+        remoteDataSource.updateDetalleReservaProyector(detalle.detalleReservaProyectorId, detalle)
+
+    suspend fun deleteDetalleReservaProyector(id: Int) =
+        remoteDataSource.deleteDetalleReservaProyector(id)
+}
