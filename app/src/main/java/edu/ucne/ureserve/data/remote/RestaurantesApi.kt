fbbe208189ed2b6076c6a379ebe65466c5a1ee3a
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 19:21:18 -0400

    RestaurantesApi.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/RestaurantesApi.kt
@@ -0,0 +1,21 @@
+package edu.ucne.ureserve.data.remote
+
+import edu.ucne.ureserve.data.remote.dto.RestaurantesDto
+import retrofit2.http.*
+
+interface RestaurantesApi {
+    @GET("api/Restaurantes")
+    suspend fun getAll(): List<RestaurantesDto>
+
+    @GET("api/Restaurantes/{id}")
+    suspend fun getById(@Path("id") id: Int): RestaurantesDto
+
+    @POST("api/Restaurantes")
+    suspend fun insert(@Body restaurante: RestaurantesDto): RestaurantesDto
+
+    @PUT("api/Restaurantes/{id}")
+    suspend fun update(@Path("id") id: Int, @Body restaurante: RestaurantesDto): RestaurantesDto
+
+    @DELETE("api/Restaurantes/{id}")
+    suspend fun delete(@Path("id") id: Int)
+}
