ccab687e86298be041165b6d16eab3c01c5eacd6
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 16:48:24 -0400

    DetalleReservaProyectorsDto

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
@@ -0,0 +1,15 @@
+package edu.ucne.ureserve.data.remote.dto
+
+data class DetalleReservaProyectorsDto(
+    val detalleReservaProyectorId: Int = 0,
+    val codigoReserva: Int = 0,
+    val idProyector: Int = 0,
+    val matricula: String = "",
+    val fecha: String = "",
+    val horario: String = "",
+    val estado: Int = 0,
+    val reservacion: ReservacionesDto = ReservacionesDto(),
+    val proyector: ProyectoresDto = ProyectoresDto(),
+    val estudiante: EstudianteDto = EstudianteDto()
+)
+
