132d8f336160df103c8496a82381914e263f1036
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-20 19:16:38 -0400

    DetalleReservaProyectorsApi.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/remote/DetalleReservaProyectorsApi.kt
@@ -0,0 +1,21 @@
+package edu.ucne.ureserve.data.remote
+
+import edu.ucne.ureserve.data.remote.dto.DetalleReservaProyectorsDto
+import retrofit2.http.*
+
+interface DetalleReservaProyectorsApi {
+    @GET("api/DetalleReservaProyectors")
+    suspend fun getAll(): List<DetalleReservaProyectorsDto>
+
+    @GET("api/DetalleReservaProyectors/{id}")
+    suspend fun getById(@Path("id") id: Int): DetalleReservaProyectorsDto
+
+    @POST("api/DetalleReservaProyectors")
+    suspend fun insert(@Body detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto
+
+    @PUT("api/DetalleReservaProyectors/{id}")
+    suspend fun update(@Path("id") id: Int, @Body detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto
+
+    @DELETE("api/DetalleReservaProyectors/{id}")
+    suspend fun delete(@Path("id") id: Int)
+}
