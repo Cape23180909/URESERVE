10b6bf2bb5ac137c47d0bf872c9264d2b96d66bc
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-21 16:58:36 -0400

    TerminosReservaRestauranteScreen.kt

--- app/src/main/java/edu/ucne/ureserve/presentation/restaurantes/TerminosReservaRestauranteScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/restaurantes/TerminosReservaRestauranteScreen.kt
@@ -7,6 +7,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +31,7 @@ fun TerminosReservaRestauranteScreen(
     onCancelarClick: () -> Unit = {},
     onBackClick: () -> Unit = {}
 ) {
+    var aceptado by remember { mutableStateOf(false) } // Empieza en false para obligar a marcar checkbox
     Scaffold(
         topBar = {
             TopAppBar(
