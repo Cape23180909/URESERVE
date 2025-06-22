83795647b611d325758f3f3d66a6fcb83995df60
Cape23180909 <samilgamer@outlook.com>
2025-06-21 23:55:43 -0400

    Ajustando el post, para que tambien se reflejen los proyectores.

--- app/src/main/java/edu/ucne/ureserve/data/remote/DetalleReservaProyectorsApi.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/DetalleReservaProyectorsApi.kt
@@ -1,6 +1,8 @@
 package edu.ucne.ureserve.data.remote
 
+import edu.ucne.ureserve.data.remote.dto.CreateDetalleReservaProyectorDto
 import edu.ucne.ureserve.data.remote.dto.DetalleReservaProyectorsDto
+import retrofit2.Response
 import retrofit2.http.*
 
 interface DetalleReservaProyectorsApi {
@@ -11,7 +13,9 @@ interface DetalleReservaProyectorsApi {
     suspend fun getById(@Path("id") id: Int): DetalleReservaProyectorsDto
 
     @POST("api/DetalleReservaProyectors")
-    suspend fun insert(@Body detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto
+    suspend fun insert(
+        @Body detalle: CreateDetalleReservaProyectorDto
+    ): Response<DetalleReservaProyectorsDto>
 
     @PUT("api/DetalleReservaProyectors/{id}")
     suspend fun update(@Path("id") id: Int, @Body detalle: DetalleReservaProyectorsDto): DetalleReservaProyectorsDto
