019ec0f556b2ba4752d01a1d096cf5468ca4fb34
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-23 13:59:40 -0400

    RestauranteReservationcalendarioScreen.kt

---
+++ app/src/main/java/edu/ucne/ureserve/presentation/restaurantes/RestauranteReservationcalendarioScreen.kt
@@ -0,0 +1,361 @@
+package edu.ucne.ureserve.presentation.restaurantes
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import edu.ucne.ureserve.R
+import edu.ucne.ureserve.presentation.dashboard.BottomNavItem
+import java.util.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RestauranteReservationcalendarioScreen(
+    onBottomNavClick: (String) -> Unit = {},
+    navController: NavController? = null
+) {
+    val calendar = Calendar.getInstance()
+    var selectedDate by remember { mutableStateOf<Calendar?>(null) }
+
+    val isSunday = selectedDate?.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY
+    val isDateValid = selectedDate != null && !isSunday
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.logo_reserve),
+                            contentDescription = "Logo",
+                            modifier = Modifier.size(60.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.icon_restaurante),
+                            contentDescription = "Restaurante",
+                            modifier = Modifier.size(60.dp)
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color(0xFF6D87A4)
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(4.dp)
+                    .background(Color(0xFF023E8A))
+            )
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF023E8A))
+                .padding(16.dp)
+        ) {
+            item {
+                HeaderSectionRestaurante()
+            }
+            item {
+                CalendarSectionRestaurante(
+                    calendar = calendar,
+                    selectedDate = selectedDate,
+                    onDateSelected = { date -> selectedDate = date }
+                )
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+
+                if (isSunday) {
+                    Text(
+                        text = "No se pueden hacer reservas los domingos",
+                        color = Color.Red,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+
+                ReservationButtonRestaurante(
+                    isEnabled = isDateValid,
+                    onClick = {
+                        println("Reserva de Restaurante para ${selectedDate?.time}")
+                    },
+                    onBottomNavClick = onBottomNavClick,
+                    onNavigateToReservaRestaurante = {
+                        val fechaSeleccionada = selectedDate?.time?.toString() ?: "Hoy"
+                        navController?.navigate("ConfirmacionReservaRestaurante?fecha=$fechaSeleccionada")
+                    },
+                    navController = navController,
+                    selectedDate = selectedDate ?: calendar
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun HeaderSectionRestaurante() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "RESTAURANTE",
+            modifier = Modifier.fillMaxWidth(),
+            style = MaterialTheme.typography.titleLarge.copy(
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+        )
+        Text(
+            text = "Mesas Disponibles: 15",
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.White)
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = "Planifique su Reserva",
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Seleccione la fecha deseada:",
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.White)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun CalendarSectionRestaurante(
+    calendar: Calendar,
+    selectedDate: Calendar?,
+    onDateSelected: (Calendar) -> Unit
+) {
+    var currentMonth by remember { mutableStateOf(calendar.get(Calendar.MONTH)) }
+    var currentYear by remember { mutableStateOf(calendar.get(Calendar.YEAR)) }
+    val tempCalendar = Calendar.getInstance().apply {
+        set(Calendar.YEAR, currentYear)
+        set(Calendar.MONTH, currentMonth)
+        set(Calendar.DAY_OF_MONTH, 1)
+    }
+    val daysInMonth = tempCalendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+    val firstDayOfWeek = tempCalendar.get(Calendar.DAY_OF_WEEK) - 1
+    val monthName = tempCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault())
+    val shortWeekdays = Array(7) { i ->
+        tempCalendar.apply { set(Calendar.DAY_OF_WEEK, i + 1) }
+            .getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.getDefault())
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton(onClick = {
+            currentMonth--
+            if (currentMonth < 0) {
+                currentMonth = 11
+                currentYear--
+            }
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.icon_left),
+                contentDescription = "Retroceder Mes",
+                tint = Color.White
+            )
+        }
+        Text(
+            text = monthName?.uppercase() ?: "",
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color.White
+            ),
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(onClick = {
+            currentMonth++
+            if (currentMonth > 11) {
+                currentMonth = 0
+                currentYear++
+            }
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.icon_right),
+                contentDescription = "Avanzar Mes",
+                tint = Color.White
+            )
+        }
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        shortWeekdays.forEach { day ->
+            Text(
+                text = day,
+                style = MaterialTheme.typography.bodySmall.copy(color = Color.White),
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+    Spacer(modifier = Modifier.height(8.dp))
+    Column {
+        var dayCounter = 1 - firstDayOfWeek
+        repeat(6) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                repeat(7) { dayOfWeek ->
+                    val day = dayCounter + dayOfWeek
+                    val isCurrentMonth = day in 1..daysInMonth
+                    val date = if (isCurrentMonth) {
+                        Calendar.getInstance().apply {
+                            set(Calendar.YEAR, currentYear)
+                            set(Calendar.MONTH, currentMonth)
+                            set(Calendar.DAY_OF_MONTH, day)
+                        }
+                    } else null
+                    val today = Calendar.getInstance()
+                    val isToday = date?.let {
+                        it.get(Calendar.YEAR) == today.get(Calendar.YEAR) &&
+                                it.get(Calendar.MONTH) == today.get(Calendar.MONTH) &&
+                                it.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH)
+                    } ?: false
+                    val isPastDate = date?.let { it.before(today) && !isToday } ?: false
+                    val isSunday = date?.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY
+                    CalendarDayRestaurante(
+                        day = if (isCurrentMonth) day.toString() else "",
+                        isSelected = selectedDate?.let {
+                            it.get(Calendar.YEAR) == date?.get(Calendar.YEAR) &&
+                                    it.get(Calendar.MONTH) == date?.get(Calendar.MONTH) &&
+                                    it.get(Calendar.DAY_OF_MONTH) == date?.get(Calendar.DAY_OF_MONTH)
+                        } ?: false,
+                        isAvailable = isCurrentMonth && !isPastDate && !isSunday,
+                        onClick = { if (isCurrentMonth && date != null && !isSunday) onDateSelected(date) }
+                    )
+                }
+            }
+            dayCounter += 7
+            if (dayCounter > daysInMonth) return@Column
+        }
+    }
+}
+
+@Composable
+private fun CalendarDayRestaurante(
+    day: String,
+    isSelected: Boolean,
+    isAvailable: Boolean,
+    onClick: () -> Unit
+) {
+    val containerColor = when {
+        isSelected -> MaterialTheme.colorScheme.primary
+        !isAvailable -> Color.Red
+        else -> Color.Transparent
+    }
+    val contentColor = when {
+        isSelected -> MaterialTheme.colorScheme.onPrimary
+        !isAvailable -> Color.White
+        else -> Color.White
+    }
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        if (day.isNotEmpty()) {
+            Surface(
+                shape = MaterialTheme.shapes.small,
+                color = containerColor,
+                onClick = onClick,
+                enabled = isAvailable
+            ) {
+                Box(
+                    modifier = Modifier.size(36.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    if (!isAvailable) {
+                        Text(
+                            text = "No\nDisponible",
+                            color = contentColor,
+                            fontSize = 12.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    } else {
+                        Text(
+                            text = day,
+                            color = contentColor,
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ReservationButtonRestaurante(
+    isEnabled: Boolean,
+    onClick: () -> Unit,
+    onBottomNavClick: (String) -> Unit,
+    onNavigateToReservaRestaurante: () -> Unit,
+    navController: NavController?,
+    selectedDate: Calendar
+) {
+    Spacer(modifier = Modifier.height(70.dp))
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFF2E5C94))
+            .padding(vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        BottomNavItem(
+            iconRes = R.drawable.icon_inicio,
+            label = "Inicio",
+            isSelected = true,
+            onClick = { onBottomNavClick("Inicio") }
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRestauranteReservationcalendarioScreen() {
+    MaterialTheme {
+        RestauranteReservationcalendarioScreen(
+            onBottomNavClick = {},
+            navController = NavController(LocalContext.current)
+        )
+    }
+}
