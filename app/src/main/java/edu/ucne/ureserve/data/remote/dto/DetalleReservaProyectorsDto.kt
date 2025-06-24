d588672ab48b3724762f026fc3b0fefbfed8437c
Cape23180909 <samilgamer@outlook.com>
2025-06-24 02:30:48 -0400

    Adactando el detalle de la reserva de proyectores, para atrapar informacion importante cuandos e haga una cicha reserva por un usuario.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
@@ -1,14 +1,36 @@
 package edu.ucne.ureserve.data.remote.dto
 
+import java.time.LocalDate
+
 data class DetalleReservaProyectorsDto(
     val detalleReservaProyectorId: Int = 0,
     val codigoReserva: Int = 0,
     val idProyector: Int = 0,
     val matricula: String = "",
-    val fecha: String = "",
-    val horario: String = "",
+    val fecha: LocalDate? = null,
+    val horario: String = "", // Usar String en formato "HH:mm"
     val estado: Int = 0,
-    val reservacion: ReservacionesDto = ReservacionesDto(),
-    val proyector: ProyectoresDto = ProyectoresDto(),
-    val estudiante: EstudianteDto = EstudianteDto()
-)
+
+    // Relaciones
+    val reservacion: ReservacionesDto? = null,
+    val proyector: ProyectoresDto? = null,
+    val estudiante: EstudianteDto? = null,
+    val usuario: UsuarioDTO? = null
+) {
+    constructor(
+        codigoReserva: Int,
+        idProyector: Int,
+        matricula: String,
+        fecha: LocalDate?,
+        horario: String,
+        estado: Int = 0
+    ) : this(
+        codigoReserva = codigoReserva,
+        idProyector = idProyector,
+        matricula = matricula,
+        fecha = fecha,
+        horario = horario,
+        estado = estado,
+        proyector = ProyectoresDto(proyectorId = idProyector)
+    )
+}
