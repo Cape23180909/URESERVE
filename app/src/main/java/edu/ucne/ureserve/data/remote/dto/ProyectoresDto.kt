71236a592cf84a415e96a8ed6e6fc4a41edaf983
Cape23180909 <samilgamer@outlook.com>
2025-06-24 05:50:46 -0400

    Quitando lo anulable a la fecha ya que no necesitare el mismo.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
@@ -7,8 +7,10 @@ data class ProyectoresDto(
     val conectividad: String = "", // HDMI, VGA, USB, etc.
 
     // Campos para relación con reservas (opcionales)
-    val fecha: String? = null, // Usando LocalDate para coincidir con DateTime de C#
-    val horario: String? = null,
+    val fecha: String = "", // Usando LocalDate para coincidir con DateTime de C#
+    val horario: String ="",
     val estado: Int = 0, // 0 = Disponible, 1 = Reservado
-    val codigoReserva: Int = 0
+    val codigoReserva: Int = 0,
+
+    val usuarioId: Int = 0
 )
