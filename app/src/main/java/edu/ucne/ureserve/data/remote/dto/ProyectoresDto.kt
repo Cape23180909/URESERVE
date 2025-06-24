714f036f25143401f67b0e61085498d4db8ed360
Cape23180909 <samilgamer@outlook.com>
2025-06-24 02:35:31 -0400

    Cmabiando las propiedades del modelo, para agregar informacion importante sobre los proyectores.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
@@ -2,8 +2,13 @@ package edu.ucne.ureserve.data.remote.dto
 
 data class ProyectoresDto(
     val proyectorId: Int = 0,
-    val fecha: String = "", // Puedes cambiar a LocalDateTime si lo deseas
-    val horario: String = "",
-    val estado: Int = 0,
+    val nombre: String = "",
+    val cantidad: Int = 0,
+    val conectividad: String = "", // HDMI, VGA, USB, etc.
+
+    // Campos para relación con reservas (opcionales)
+    val fecha: String? = null, // Usando LocalDate para coincidir con DateTime de C#
+    val horario: String? = null,
+    val estado: Int = 0, // 0 = Disponible, 1 = Reservado
     val codigoReserva: Int = 0
 )
