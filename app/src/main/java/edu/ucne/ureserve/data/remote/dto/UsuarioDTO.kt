87d68d150a703076062e1059c71e3a9e778e4a54
Cape23180909 <samilgamer@outlook.com>
2025-06-26 18:16:48 -0400

    Agregando el seriable para que los datos de este Dto,se traslanden de manera efectiva como el servidor y la base de datos.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/UsuarioDTO.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/UsuarioDTO.kt
@@ -1,5 +1,8 @@
 package edu.ucne.ureserve.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UsuarioDTO (
     val usuarioId: Int = 0,
     val nombres: String = "",
