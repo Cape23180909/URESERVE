a750dff9d3c0a04745c92b3062e3b8abd0c31f23
Cape23180909 <samilgamer@outlook.com>
2025-06-26 18:11:55 -0400

    Colocando el Seriable, para permitir el trafico entre el servidor y dispositivos sin perder los datos y la entidad en ese caso que es de ReservacionesDto.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/ReservacionesDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/ReservacionesDto.kt
@@ -1,11 +1,14 @@
 package edu.ucne.ureserve.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ReservacionesDto(
     val reservacionId: Int = 0,
     val codigoReserva: Int = 0,
     val tipoReserva: Int = 0,
     val cantidadEstudiantes: Int = 0,
-    val fecha: String = "", // o usa LocalDateTime si lo vas a parsear
+    val fecha: String = "",
     val horario: String = "",
     val estado: Int = 0,
     val matricula: String = ""
