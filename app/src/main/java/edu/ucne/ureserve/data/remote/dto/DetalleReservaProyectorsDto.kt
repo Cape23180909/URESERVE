a2794efd20eb32acf033f24dd1417271365f3f8c
Cape23180909 <samilgamer@outlook.com>
2025-06-26 17:53:46 -0400

    Colocando el Dto, mas limpio, para que los datos se consuman exelente en dicha api, y colocando la Serializable para que guarde los datos mas facil en el mismo y me envito hacer conversiones manuales que es loq ue hago aqui ene ste Dto, para tener el codigo limpio.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
@@ -1,36 +1,15 @@
 package edu.ucne.ureserve.data.remote.dto
 
-import java.time.LocalDate
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class DetalleReservaProyectorsDto(
-    val detalleReservaProyectorId: Int = 0,
-    val codigoReserva: Int = 0,
-    val idProyector: Int = 0,
-    val matricula: String = "",
-    val fecha: LocalDate? = null,
-    val horario: String = "", // Usar String en formato "HH:mm"
-    val estado: Int = 0,
-
-    // Relaciones
-    val reservacion: ReservacionesDto? = null,
-    val proyector: ProyectoresDto? = null,
-    val estudiante: EstudianteDto? = null,
-    val usuario: UsuarioDTO? = null
-) {
-    constructor(
-        codigoReserva: Int,
-        idProyector: Int,
-        matricula: String,
-        fecha: LocalDate?,
-        horario: String,
-        estado: Int = 0
-    ) : this(
-        codigoReserva = codigoReserva,
-        idProyector = idProyector,
-        matricula = matricula,
-        fecha = fecha,
-        horario = horario,
-        estado = estado,
-        proyector = ProyectoresDto(proyectorId = idProyector)
-    )
-}
+    val detalleReservaProyectorId: Int,
+    val codigoReserva: Int,
+    val idProyector: Int,
+    val matricula: String,
+    val fecha: String,
+    val horario: String,
+    val estado: Int,
+    val proyector: ProyectoresDto
+)
