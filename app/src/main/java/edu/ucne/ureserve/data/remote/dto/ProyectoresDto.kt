a8a7d49b12b6ad594adf84a87a8801421051e6b6
Cape23180909 <samilgamer@outlook.com>
2025-06-26 17:58:01 -0400

    Poniendo el Dto de ProyectoresDto, limpio, para que se conecte bien con mi json del Swagger.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/ProyectoresDto.kt
@@ -4,16 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ProyectoresDto(
-    val proyectorId: Int = 0,
-    val nombre: String = "",
-    val cantidad: Int = 0,
-    val conectividad: String = "", // HDMI, VGA, USB, etc.
-
-    // Campos para relación con reservas (opcionales)
-    val fecha: String = "", // Usando LocalDate para coincidir con DateTime de C#
-    val horario: String ="",
-    val estado: Int = 0, // 0 = Disponible, 1 = Reservado
-    val codigoReserva: Int = 0,
-
-    val usuarioId: Int = 0
+    val proyectorId: Int,
+    val nombre: String,
+    val cantidad: Int,
+    val conectividad: String
 )
