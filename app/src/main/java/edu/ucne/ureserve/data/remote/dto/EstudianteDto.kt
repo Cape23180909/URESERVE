f45071616e9c8922bbb503dfa926f2fe44af2881
Cape23180909 <samilgamer@outlook.com>
2025-06-26 17:57:04 -0400

    Colocando el Serializable, para no perder informacion en el camino cuando se este recibiendo o enviando.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/EstudianteDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/EstudianteDto.kt
@@ -1,5 +1,8 @@
 package edu.ucne.ureserve.data.remote.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EstudianteDto (
     val estudianteId: Int = 0,
     val matricula: String = "",
