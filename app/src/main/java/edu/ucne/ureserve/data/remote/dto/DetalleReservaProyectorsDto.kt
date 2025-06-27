2bf58f385fd2567489ed17b7f292f3ebac98e927
Cape23180909 <samilgamer@outlook.com>
2025-06-27 15:44:10 -0400

    Cambiando la proiedad, proyector, para que nose llama, sino que solo se llame el Id solamente.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaProyectorsDto.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DetalleReservaProyectorsDto(
-    val detalleReservaProyectorId: Int,
+    val detalleReservaProyectorId: Int = 0,
     val codigoReserva: Int,
     val idProyector: Int,
     val matricula: String,
     val fecha: String,
     val horario: String,
     val estado: Int,
-    val proyector: ProyectoresDto
+    val proyectorId: Int? = null
 )
