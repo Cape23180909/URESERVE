47ee22117442ddec7e9a652f7c2506143305d6b8
Cape23180909 <samilgamer@outlook.com>
2025-06-28 00:48:57 -0400

    Colocando las propiedades correctas que espere el json de este modelo.

--- app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaCubiculosDto.kt
+++ app/src/main/java/edu/ucne/ureserve/data/remote/dto/DetalleReservaCubiculosDto.kt
@@ -2,15 +2,11 @@ package edu.ucne.ureserve.data.remote.dto
 
 data class DetalleReservaCubiculosDto(
     val detalleReservaCubiculoId: Int = 0,
-    val codigoReserva: Int = 0,
-    val idCubiculo: Int = 0,
-    val matricula: String = "",
-    val fecha: String = "",
-    val horario: String = "",
+    val codigoReserva: Int,
+    val idCubiculo: Int,
+    val matricula: String,
+    val fecha: String,
+    val horario: String,
     val cantidadEstudiantes: Int = 0,
-    val estado: Int = 0,
-    val reservacion: ReservacionesDto = ReservacionesDto(),
-    val cubiculo: CubiculosDto = CubiculosDto(),
-    val estudiante: EstudianteDto = EstudianteDto()
+    val estado: Int,
 )
-
