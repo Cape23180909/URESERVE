61197e58e1bc491e9296cca4b869246f53068fe0
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-06-29 09:26:40 -0400

    main activity

--- app/src/main/java/edu/ucne/ureserve/MainActivity.kt
+++ app/src/main/java/edu/ucne/ureserve/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.ucne.ureserve
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +23,7 @@ import edu.ucne.ureserve.ui.theme.URESERVETheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
