63095030f932144d57d582bf7ece9f367a960d81
Cape23180909 <samilgamer@outlook.com>
2025-07-14 07:32:46 -0400

    Agregando un nuevo metodo al repository, llamado "buscarusuarioPorLaMatricula", para que asi solo el usuario solo tenga que dijitar su matricula.

--- app/src/main/java/edu/ucne/ureserve/data/repository/LaboratorioRepository.kt
+++ app/src/main/java/edu/ucne/ureserve/data/repository/LaboratorioRepository.kt
@@ -3,14 +3,17 @@ package edu.ucne.ureserve.data.repository
 import android.util.Log
 import edu.ucne.ureserve.data.remote.RemoteDataSource
 import edu.ucne.ureserve.data.remote.Resource
+import edu.ucne.ureserve.data.remote.UsuarioApi
 import edu.ucne.ureserve.data.remote.dto.LaboratoriosDto
+import edu.ucne.ureserve.data.remote.dto.UsuarioDTO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
 import javax.inject.Inject
 
 class LaboratorioRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val usuarioApi: UsuarioApi
 ) {
     fun getLaboratorios(): Flow<Resource<List<LaboratoriosDto>>> = flow {
         try {
@@ -23,6 +26,27 @@ class LaboratorioRepository @Inject constructor(
         } catch (e: Exception) {
             Log.e("Retrofit Unknown", "Error desconocido ${e.message}", e)
             emit(Resource.Error("Error desconocido: ${e.message}"))
+        }
+    }
+
+    suspend fun buscarUsuarioPorMatricula(matricula: String): UsuarioDTO? {
+        return try {
+            val usuarios = usuarioApi.getAll()
+            Log.d("Repository", "Usuarios recuperados: ${usuarios.size}")
+            val normalizedMatricula = matricula.replace("-", "")
+            val usuario = usuarios.find { usuario ->
+                val userMatricula = usuario.estudiante?.matricula?.replace("-", "") ?: ""
+                userMatricula.equals(normalizedMatricula, ignoreCase = true)
+            }
+            if (usuario != null) {
+                Log.d("Repository", "Usuario encontrado: ${usuario.nombres}")
+            } else {
+                Log.d("Repository", "Usuario no encontrado para la matrícula: $matricula")
+            }
+            usuario
+        } catch (e: Exception) {
+            Log.e("Repository", "Error buscando usuario", e)
+            null
         }
     }
 
