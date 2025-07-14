0098711aba06c0902bf3c0e096ad124e53c545c8
Cape23180909 <samilgamer@outlook.com>
2025-07-14 07:35:39 -0400

    Optimizando los imports de este Screen.

--- app/src/main/java/edu/ucne/ureserve/presentation/cubiculos/AgregarEstudianteScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/cubiculos/AgregarEstudianteScreen.kt
@@ -14,13 +14,13 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import edu.ucne.ureserve.R
 import edu.ucne.ureserve.presentation.cubiculos.ReservaCubiculoViewModel
+import edu.ucne.ureserve.presentation.laboratorios.ReservaLaboratorioViewModel
 
 @Composable
 fun AgregarEstudianteScreen(
