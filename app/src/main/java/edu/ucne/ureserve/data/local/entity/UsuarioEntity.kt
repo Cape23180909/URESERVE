4a02d1b2f4dc9eb5ce315175d11246ae9dc9cd8d
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-26 17:24:58 -0400

    agregar entidad UsuarioEntity con mapeo a DTO

--- app/src/main/java/edu/ucne/ureserve/data/local/entity/UsuarioEntity.kt
+++ app/src/main/java/edu/ucne/ureserve/data/local/entity/UsuarioEntity.kt
@@ -2,6 +2,7 @@ package edu.ucne.ureserve.data.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import edu.ucne.ureserve.data.remote.dto.UsuarioDTO
 
 @Entity(tableName = "Usuarios")
 data class UsuarioEntity(
@@ -13,3 +14,24 @@ data class UsuarioEntity(
     val clave: String = "",
     val estudianteId: Int = 0
 )
+fun UsuarioDTO.toEntity(): UsuarioEntity {
+    return UsuarioEntity(
+        usuarioId = usuarioId,
+        nombres = nombres,
+        apellidos = apellidos,
+        correoInstitucional = correoInstitucional,
+        clave = clave,
+        estudianteId = estudianteId
+    )
+}
+
+fun UsuarioEntity.toDto(): UsuarioDTO {
+    return UsuarioDTO(
+        usuarioId = usuarioId,
+        nombres = nombres,
+        apellidos = apellidos,
+        correoInstitucional = correoInstitucional,
+        clave = clave,
+        estudianteId = estudianteId
+    )
+}
