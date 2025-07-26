1002ebe47d09f62d2b69abaff766a559bfc66101
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-26 14:14:05 -0400

    Le agregue el  UsuarioDao.kt

---
+++ app/src/main/java/edu/ucne/ureserve/data/local/dao/UsuarioDao.kt
@@ -0,0 +1,28 @@
+package edu.ucne.ureserve.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.ureserve.data.local.entity.UsuarioEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UsuarioDao {
+    @Upsert
+    suspend fun save(usuario: UsuarioEntity)
+
+    @Query("""
+        SELECT *
+        FROM Usuarios
+        WHERE usuarioId = :id
+        LIMIT 1
+    """)
+    suspend fun find(id: Int?): UsuarioEntity?
+
+    @Delete
+    suspend fun delete(usuario: UsuarioEntity)
+
+    @Query("SELECT * FROM Usuarios")
+    fun getAll(): Flow<List<UsuarioEntity>>
+}
