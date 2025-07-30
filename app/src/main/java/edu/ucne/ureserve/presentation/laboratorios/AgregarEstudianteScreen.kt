a7483df765a989948e2056fbc2f5c24df3354580
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-07-30 09:38:35 -0400

    Le agregue  la notificacion a AgregarEstudianteScreen.kt

--- app/src/main/java/edu/ucne/ureserve/presentation/laboratorios/AgregarEstudianteScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/laboratorios/AgregarEstudianteScreen.kt
@@ -1,5 +1,7 @@
 package edu.ucne.ureserve.presentation.laboratorios
 
+import android.Manifest
+import android.os.Build
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -35,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -46,9 +49,14 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import edu.ucne.registrotecnicos.common.NotificationHandler
 import edu.ucne.ureserve.R
 import edu.ucne.ureserve.presentation.cubiculos.ReservaCubiculoViewModel
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun AgregarEstudianteScreenLaboratorio(
     viewModel: ReservaLaboratorioViewModel = hiltViewModel(),
@@ -56,6 +64,23 @@ fun AgregarEstudianteScreenLaboratorio(
     onCancel: () -> Unit = {},
     onAdd: (String) -> Unit = {}
 ){
+    val context = LocalContext.current
+
+
+    // Solicitud de permiso para notificaciones en Android 13+
+    val postNotificationPermission =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            rememberPermissionState(permission = Manifest.permission.POST_NOTIFICATIONS)
+        } else null
+
+    val notificationHandler = remember { NotificationHandler(context) }
+
+    LaunchedEffect(true) {
+        if (postNotificationPermission != null && !postNotificationPermission.status.isGranted) {
+            postNotificationPermission.launchPermissionRequest()
+        }
+    }
+
     var matricula by remember { mutableStateOf("") }
     val errorMessage by viewModel.errorMessage.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
@@ -180,6 +205,12 @@ fun AgregarEstudianteScreenLaboratorio(
                                 viewModel.buscarUsuarioPorMatricula(matriculaLimpia) { usuarioEncontrado ->
                                     if (usuarioEncontrado != null) {
                                         viewModel.addMember(usuarioEncontrado)
+                                        //  Mostrar notificación al agregar el estudiante
+                                        notificationHandler.showNotification(
+                                            title = "Estudiante Agregado",
+                                            message = "El estudiante con matrícula $matricula fue agregado correctamente."
+                                        )
+
                                         navController.popBackStack()
                                     } else {
                                         viewModel.setError("Matrícula no válida")
