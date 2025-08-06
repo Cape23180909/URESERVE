3c1f783a5335257bcf8cad9a07300dc321bc95dd
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-08-06 17:48:33 -0400

    Termiando de hacer el Uistate de laboratorios

---
+++ app/src/main/java/edu/ucne/ureserve/presentation/laboratorios/ReservaLaboratorioUiState.kt
@@ -0,0 +1,25 @@
+package edu.ucne.ureserve.presentation.laboratorios
+
+import edu.ucne.ureserve.data.remote.dto.UsuarioDTO
+import java.time.LocalDate
+import java.time.LocalTime
+
+data class ReservaLaboratorioUiState(
+    val reservaId: Int? = null,
+    val codigoReserva: Int? = null,
+    val tipoReserva: Int = 3, // 3 = Laboratorio
+    val cantidadEstudiantes: Int = 0,
+    val fecha: LocalDate? = null,
+    val horaInicio: LocalTime? = null,
+    val horaFin: LocalTime? = null,
+    val estado: Int = 1,
+    val matricula: String = "",
+    val laboratorioId: Int? = null,
+    val laboratorioNombre: String = "",
+    val miembros: List<UsuarioDTO> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null,
+    val fechaString: String = "",
+    val horaInicioString: String = "",
+    val horaFinString: String = ""
+)
