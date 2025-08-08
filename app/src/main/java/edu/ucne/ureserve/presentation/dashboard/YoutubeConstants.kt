8a303079e770bccbff4ea19cb3e24caff44bdc2b
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-08-08 15:29:56 -0400

    La cambie la URL

--- app/src/main/java/edu/ucne/ureserve/presentation/dashboard/YoutubeConstants.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/dashboard/YoutubeConstants.kt
@@ -3,6 +3,6 @@ package edu.ucne.ureserve.presentation.dashboard
 import android.webkit.WebSettings
 
 object YoutubeConstants {
-    const val YOUTUBE_URL = "https://www.youtube.com/?app=desktop&hl=es"
+    const val YOUTUBE_URL = "https://youtube.com/playlist?list=PLmKX4EsU0GpXaqUVf7xU7zv8oKpoGeEeq&si=bgsiRIjkKd3PLM3f"
     const val CACHE_MODE = WebSettings.LOAD_DEFAULT
 }
