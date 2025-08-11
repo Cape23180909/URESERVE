5d5d1e9b10396b8b13e80ab6ecc5cddb113fd8c0
Cape23180909 <168696104+Cape23180909@users.noreply.github.com>
2025-08-11 08:41:08 -0400

    Colocando las implementaciones del boton iprimir como el decargar de la pantalla ReporteGeneralListScreen.kt

--- app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteGeneralListScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteGeneralListScreen.kt
@@ -1,5 +1,6 @@
 package edu.ucne.ureserve.presentation.reportes
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -28,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -41,6 +43,7 @@ fun ReporteGeneralListScreen(
     navController: NavController,
     viewModel: ReporteViewModel = hiltViewModel()
 ){
+    val context = LocalContext.current
     val state = viewModel.uiState.collectAsState().value
     LaunchedEffect(Unit) {
         viewModel.loadReservas()
@@ -181,8 +184,34 @@ fun ReporteGeneralListScreen(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                ActionButton("DESCARGAR", Color(0xFF007ACC)) { /* acción descargar */ }
-                ActionButton("IMPRIMIR", Color(0xFF00B4D8)) { /* acción imprimir */ }
+                ActionButton("DESCARGAR", Color(0xFF007ACC)) {
+                    if (state.reservas.isNotEmpty()) {
+                        val pdfFile = generarPdfReservasGeneral(context, state.reservas)
+                        Toast.makeText(
+                            context,
+                            "PDF guardado en: ${pdfFile.absolutePath}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "No hay reservas para generar PDF",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                ActionButton("IMPRIMIR", Color(0xFF00B4D8)) {
+                    if (state.reservas.isNotEmpty()) {
+                        imprimirPdfGeneral(context, state.reservas)
+                    } else {
+                        Toast.makeText(
+                            context,
+                            "No hay reservas para imprimir",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
