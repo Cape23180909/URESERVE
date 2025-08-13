61694a6248fb59a03ea7d25b21ea57852786b0ee
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-08-13 09:16:35 -0400

    Limpiando el ReporteLaboratorioScreen.kt para corregir dichos issues del sonar

--- app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteLaboratorioScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteLaboratorioScreen.kt
@@ -142,7 +142,7 @@ fun ReporteLaboratoriosScreen(
                                         ReservationDetailBlock("NO. RESERVA", reserva.codigoReserva.toString())
                                         ReservationDetailBlock("FECHA", reserva.fechaFormateada)
                                         ReservationDetailBlock("HORARIO", "${reserva.horaInicio} a ${reserva.horaFin}")
-                                        ReservationDetailBlock("MATRÍCULA", reserva.matricula)
+                                        ReservationDetailBlock("MATRÍCULA", reserva.matricula!!)
                                         Spacer(modifier = Modifier.height(8.dp))
                                     }
                                 }
