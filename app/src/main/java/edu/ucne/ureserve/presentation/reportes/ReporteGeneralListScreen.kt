00d48d1bedcdbcd248b2a7f2550755c002e05622
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-08-13 09:16:12 -0400

    Limpiando el ReporteGeneralListScreen.kt para corregir dichos issues del sonar

--- app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteGeneralListScreen.kt
+++ app/src/main/java/edu/ucne/ureserve/presentation/reportes/ReporteGeneralListScreen.kt
@@ -154,7 +154,7 @@ fun ReporteGeneralListScreen(
                     ReservationDetailBlock("NO. RESERVA", reserva.codigoReserva.toString())
                     ReservationDetailBlock("FECHA", reserva.fechaFormateada)
                     ReservationDetailBlock("HORARIO", "${reserva.horaInicio} a ${reserva.horaFin}")
-                    ReservationDetailBlock("MATRÍCULA", reserva.matricula)
+                    ReservationDetailBlock("MATRÍCULA", reserva.matricula!!)
                     Spacer(modifier = Modifier.height(8.dp))
                 }
             }
